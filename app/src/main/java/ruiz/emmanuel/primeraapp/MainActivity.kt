412ae12098653c9ba979b930a520cc209ac7ef20
EMMANUEL RUIZ GARIBAY <emmanuel.ruiz0221@alumnos.udg.mx>
2022-01-23 17:12:57 -0600

    Hola mundo Emmanuel

--- app/src/main/java/ruiz/emmanuel/primeraapp/MainActivity.kt
+++ app/src/main/java/ruiz/emmanuel/primeraapp/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Primera app del curso "Emmanuel Ruiz Garibay 216022179"
     }
 }
